ef365eba47b7548c96f2d4951aa3b5af6bf2e7a4
prasannakarki77 <prasanna.karki77@gmail.com>
2022-11-07 14:06:49 +0545

    created components and designed navbar

---
+++ src/components/layout/Header.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { useState } from "react";
+import logo from "../../assets/logo.svg";
+import "./header.scss";
+const Header = () => {
+  const [openMenu, setOpenMenu] = useState(false);
+  const menuClickHandler = (e) => {
+    e.preventDefault();
+    setOpenMenu((p) => !p);
+  };
+  return (
+    <div className="header">
+      <div className="header__logo">
+        <img src={logo} alt="logo" className="header__logo--img" />
+      </div>
+      <div
+        className={`menu ${openMenu ? "menu--open" : ""}`}
+        onClick={menuClickHandler}
+      >
+        <div className="menu__line "></div>
+        <div className="menu__line "></div>
+        <div className="menu__line  "></div>
+      </div>
+      <nav className={`nav ${openMenu ? "nav--slide" : ""}`}>
+        <Link className="nav__links ">Home</Link>
+        <Link className="nav__links">Books</Link>
+        <Link className="nav__links">About</Link>
+        <Link className="nav__btn nav__links">Sign In</Link>
+      </nav>
+    </div>
+  );
+};
+
+export default Header;
