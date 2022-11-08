8ffa1b6fd7b7da61a75a4cb07da329c7c3caf063
prasannakarki77 <prasanna.karki77@gmail.com>
2022-11-08 08:38:48 +0545

    designed homepage hero section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,33 @@
 import React from "react";
-
+import "./home.scss";
+import { BiSearch } from "react-icons/bi";
+import { FaBook } from "react-icons/fa";
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className="home-container">
+      <section className="hero">
+        <form className="search">
+          <input
+            type="text"
+            className="search__input"
+            placeholder="Enter book name, author....."
+          />
+          <button className="search__btn">
+            <BiSearch />
+            Search
+          </button>
+        </form>
+        <h2 className="hero__text">
+          A book worm’s paradise <br />{" "}
+          <span className="hero__text--red"> RENT</span> &{" "}
+          <span className="hero__text--green">EXCHANGE</span>
+        </h2>
+        <button className="hero__btn">
+          List Books for Rent <FaBook />
+        </button>
+      </section>
+    </div>
+  );
 };
 
 export default Home;
