12a644bd632eecdab041b4ceff209d4dfe568d0a
aayush <aayush172@gmail.com>
2022-11-09 17:21:39 +0545

    Register Api Endpoint Complete

--- src/components/register/Register.jsx
+++ src/components/register/Register.jsx
@@ -6,7 +6,42 @@ import { ImKey } from "react-icons/im";
 import { AiFillGoogleCircle } from "react-icons/ai";
 import "./register.scss";
 import { Link } from "react-router-dom";
+
+import axios from "axios";
+import { useState } from "react";
+
 const Register = () => {
+  const [username, setUsername] = useState("");
+
+  const [email, setEmail] = useState("");
+
+  const [contact_no, setContactNo] = useState("");
+
+  const [password, setPassword] = useState("");
+
+  const registerUser = (e) => {
+    e.preventDefault();
+
+    const data = {
+      username: username,
+      contact_no: contact_no,
+      email: email,
+      password: password,
+    };
+
+    axios
+      .post("http://localhost:90/user/register", data)
+      .then((response) => {
+        if (response.data.msg === "User Registered Successfully") {
+          console.log("User Registered Successfully", response);
+        } else {
+          console.log(response);
+        }
+      })
+
+      .catch();
+  };
+
   return (
     <div className="register-container">
       <div className="register">
@@ -24,21 +59,33 @@ const Register = () => {
           <form>
             <div className="register__input">
               <FaUserAlt size={20} className="register__input--icon" />
-              <input type="text" placeholder="Username" />
+              <input type="text" placeholder="Username" 
+              onChange={(e) => {
+                setUsername(e.target.value);
+              }}/>
             </div>
             <div className="register__input">
               <MdEmail size={20} className="register__input--icon" />
-              <input type="email" placeholder="Email address" />
+              <input type="email" placeholder="Email address" 
+              onChange={(e) => {
+                setEmail(e.target.value);
+              }}/>
             </div>
             <div className="register__input">
               <FaPhone size={20} className="register__input--icon" />
-              <input type="number" placeholder="Contact No." />
+              <input type="number" placeholder="Contact No." 
+              onChange={(e) => {
+                setContactNo(e.target.value);
+              }}/>
             </div>
             <div className="register__input">
               <ImKey size={20} className="register__input--icon" />
-              <input type="password" placeholder="Enter password" />
+              <input type="password" placeholder="Enter password" 
+              onChange={(e) => {
+                setPassword(e.target.value);
+              }}/>
             </div>
-            <button type="submit" className="register__btn">
+            <button type="submit" className="register__btn" onClick={registerUser}>
               Create an account
             </button>
           </form>
