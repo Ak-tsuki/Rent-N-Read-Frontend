7cd295f317838c06c89a8d688511d3bf00173b5a
aaronthaku <me9aaron@gmail.com>
2022-11-17 21:48:21 +0545

    Final frontend of approve book

--- src/components/admin-approveBook/AdminApprove.jsx
+++ src/components/admin-approveBook/AdminApprove.jsx
@@ -1,78 +1,110 @@
 import React from "react";
 import Table from "react-bootstrap/Table";
 import "./admin_approve.scss";
+import { BsFillCheckCircleFill } from "react-icons/bs";
+import { ImCross } from "react-icons/im";
 
 const AdminApprove = () => {
   return (
     <div>
-      <Table responsive="sm" id="heading" striped>
-        <thead>
-          <caption id="title">Verified Books</caption>
-          <tr>
-            <th>SN.</th>
-            <th>Book Image</th>
-            <th>Name</th>
-            <th>Description</th>
-            <th>Author</th>
-            <th>Category</th>
-            <th>Rent</th>
-            <th>Book Owner</th>
-            <th>Action</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>1</td>
-            <td class="w-25">
-              <img
-                src="https://m.media-amazon.com/images/M/MV5BMTY4NTIwODg0N15BMl5BanBnXkFtZTcwOTc0MjEzMw@@._V1_.jpg"
-                class="img-fluid img-thumbnail"
-                alt="Sheep"
-              />
-            </td>
-            <td>Harry Portter</td>
-            <td>This is the book of...</td>
-            <td>Jk Rolling</td>
-            <td>Fantacy</td>
-            <td>Rs. 50</td>
-            <td>Hari</td>
-            <td>
-              <button class="approve--btn">Approved</button>
-              <button class="reject--btn ">Reject</button>
-            </td>
-          </tr>
+      <h1 class="table-title">Verify Books</h1>
+      <div className="table-responsive">
+        <Table className="table" id="heading" striped>
+          <thead>
+            <tr>
+              <th>SN.</th>
+              <th>Book Image</th>
+              <th>Name</th>
+              <th>Description</th>
+              <th>Author</th>
+              <th>Category</th>
+              <th>Rent</th>
+              <th>Book Owner</th>
+              <th>Status</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>1</td>
+              <td class="w-25">
+                <img
+                  src="https://m.media-amazon.com/images/M/MV5BMTY4NTIwODg0N15BMl5BanBnXkFtZTcwOTc0MjEzMw@@._V1_.jpg"
+                  class="img-fluid img-thumbnail table-img"
+                  alt="Sheep"
+                />
+              </td>
+              <td>Harry Portter</td>
+              <td>This is the book of...</td>
+              <td>Jk Rolling</td>
+              <td>Fantacy</td>
+              <td>Rs. 50</td>
+              <td>Hari</td>
+              <td className="pending-color">Pending</td>
+              <td>
+                <button class="approve--btn">
+                  Approve &nbsp; <BsFillCheckCircleFill />
+                </button>
+                <button class="reject--btn ">
+                  Reject &nbsp; <ImCross />
+                </button>
+              </td>
+            </tr>
 
-          <tr>
-            <td>2</td>
-            <td>Mark</td>
-            <td>Harry Portter</td>
-            <td>This is the book of...</td>
-            <td>Jk Rolling</td>
-            <td>Fantacy</td>
-            <td>Rs. 50</td>
-            <td>Hari</td>
-            <td>
-              <button>Approved</button>
-              <button>Approved</button>
-            </td>
-          </tr>
+            <tr>
+              <td>2</td>
+              <td class="w-25">
+                <img
+                  src="https://m.media-amazon.com/images/M/MV5BMTY4NTIwODg0N15BMl5BanBnXkFtZTcwOTc0MjEzMw@@._V1_.jpg"
+                  class="img-fluid img-thumbnail table-img"
+                  alt="Sheep"
+                />
+              </td>
+              <td>Harry Portter</td>
+              <td>This is the book of...</td>
+              <td>Jk Rolling</td>
+              <td>Fantacy</td>
+              <td>Rs. 50</td>
+              <td>Hari</td>
+              <td className="pending-color">Pending</td>
+              <td>
+                <button class="approve--btn">
+                  Approve &nbsp; <BsFillCheckCircleFill />
+                </button>
+                <button class="reject--btn ">
+                  Reject &nbsp; <ImCross />
+                </button>
+              </td>
+            </tr>
 
-          <tr>
-            <td>3</td>
-            <td>Mark</td>
-            <td>Harry Portter</td>
-            <td>This is the book of...</td>
-            <td>Jk Rolling</td>
-            <td>Fantacy</td>
-            <td>Rs. 50</td>
-            <td>Hari</td>
-            <td>
-              <button>Approved</button>
-              <button>Approved</button>
-            </td>
-          </tr>
-        </tbody>
-      </Table>
+            <tr>
+              <td>3</td>
+              <td class="w-25">
+                <img
+                  src="https://m.media-amazon.com/images/M/MV5BMTY4NTIwODg0N15BMl5BanBnXkFtZTcwOTc0MjEzMw@@._V1_.jpg"
+                  class="img-fluid img-thumbnail table-img"
+                  alt="Sheep"
+                />
+              </td>
+              <td>Harry Portter</td>
+              <td>This is the book of...</td>
+              <td>Jk Rolling</td>
+              <td>Fantacy</td>
+              <td>Rs. 50</td>
+              <td>Hari</td>
+              <td className="pending-color">Pending</td>
+              <td>
+                <button class="approve--btn">
+                  Approve &nbsp; <BsFillCheckCircleFill />
+                </button>
+                <button class="reject--btn ">
+                  Reject &nbsp; <ImCross />
+                </button>
+              </td>
+            </tr>
+          </tbody>
+        </Table>
+      </div>
     </div>
   );
 };
--- src/components/book-card/BookCard.jsx
+++ src/components/book-card/BookCard.jsx
@@ -2,21 +2,40 @@ import React from "react";
 import "./bookCard.scss";
 import { FaTrash, FaPenAlt } from "react-icons/fa";
 const BookCard = ({ book }) => {
-  const { name, author, rent_cost_perday, book_pic, desc, category, status } = book;
+  const { name, author, rent_cost_perday, book_pic, desc, category, status } =
+    book;
   return (
     <div className="book-card">
       <FaTrash className="book-card__delete" />
-      <img src={`http://localhost:90/${book_pic}`} alt="book_img" className="book-card__img" />
+      <img
+        src={`http://localhost:90/${book_pic}`}
+        alt="book_img"
+        className="book-card__img"
+      />
       <div className="book-details">
         <h2 className="book-details__title">{name}</h2>
         <p className="book-details__author">{author}</p>
         <p className="book-details__desc">{desc}</p>
         <p className="book-details__desc">{category.toString()}</p>
-        
-        <p className="book-details__desc">Status:<span className={`book-details__desc  ${ status==="Requested" && "text-warning" || status==="Approved" && "text-success" || status==="Rejected" && "text-danger"}`}> {status}</span></p>
+
+        <p className="book-details__desc">
+          Status:
+          <span
+            className={`book-details__desc  ${
+              (status === "Pending" && "text-warning") ||
+              (status === "Approved" && "text-success") ||
+              (status === "Rejected" && "text-danger")
+            }`}
+          >
+            {" "}
+            {status}
+          </span>
+        </p>
         <p className="book-details__cost">
           Rent Cost:{" "}
-          <span className="book-details__cost--amount">Rs {rent_cost_perday}</span>
+          <span className="book-details__cost--amount">
+            Rs {rent_cost_perday}
+          </span>
           /day
         </p>
         <div className="book-details__update">
