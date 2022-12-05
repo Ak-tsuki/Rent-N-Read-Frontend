35d72219ffabc60f55734f1c3bc0938c032c2727
tsering-hub <ts19sherpa@gmail.com>
2022-12-05 16:40:05 +0545

    reject button popup model added

--- src/components/exchangebook-card/RequestedExchangeCard.jsx
+++ src/components/exchangebook-card/RequestedExchangeCard.jsx
@@ -2,17 +2,21 @@ import React from "react";
 import "./exchangebookcard.scss";
 import { FaTrash } from "react-icons/fa";
 import { FiSend } from "react-icons/fi";
-
 import { TiCancel } from "react-icons/ti";
-
+import Modal from "@mui/material/Modal";
+import Box from "@mui/material/Box";
 import axios from "axios";
 import { toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import moment from "moment";
 import { GiReturnArrow } from "react-icons/gi";
-
+import { Typography } from "@mui/material";
+import { ImCross } from "react-icons/im";
+import { BsCheckLg } from "react-icons/bs";
 const RequestedExchangeCard = ({ book }) => {
-  console.log(book);
+  const [view, setView] = React.useState(false);
+  const handleOpen = () => setView(true);
+  const handleClose = () => setView(false);
 
   const {
     bookId,
@@ -21,6 +25,18 @@ const RequestedExchangeCard = ({ book }) => {
     exchangeStatus,
     requestedUserId,
   } = book;
+
+  const style = {
+    position: "absolute",
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%, -50%)",
+    width: "70%",
+    bgcolor: "background.paper",
+    borderRadius: "10px",
+    boxShadow: 24,
+    p: 4,
+  };
 
   const config = {
     headers: {
@@ -115,9 +131,7 @@ const RequestedExchangeCard = ({ book }) => {
             </button>
             <button
               className="btn btn-danger m-2"
-              onClick={(e) => {
-                rejectrequest(book._id, e);
-              }}
+              onClick={handleOpen}
               data-test="btn-reject"
             >
               Reject It <TiCancel className="ms-1 fs-4" />
@@ -127,6 +141,35 @@ const RequestedExchangeCard = ({ book }) => {
           <div></div>
         )}
       </div>
+      <Modal
+        open={view}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+        data-test="delete-modal"
+      >
+        <Box sx={style}>
+          <Typography id="modal-modal-title" variant="h6" component="h2">
+            Are you sure you want to reject this Exchange request?
+          </Typography>
+          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+            <div className="d-flex align-items-center ">
+              <button
+                className="approve--btn"
+                onClick={(e) => {
+                  rejectrequest(book._id, e);
+                }}
+                data-test="yes-btn"
+              >
+                Yes &nbsp; <BsCheckLg />
+              </button>
+              <button onClick={handleClose} className="reject--btn ">
+                No &nbsp; <ImCross />
+              </button>
+            </div>
+          </Typography>
+        </Box>
+      </Modal>
     </div>
   );
 };
