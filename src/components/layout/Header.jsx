648f73ec1c1854750131566111c0347cc20f2e81
tsering-hub <ts19sherpa@gmail.com>
2022-12-22 16:58:31 +0545

    thrid party login with google successful

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -11,37 +11,8 @@ const Header = () => {
   };
   const logout = () => {
     localStorage.clear();
-    window.location.replace("/login");
+    window.open("http://localhost:90/thirdpartyRouter/logout", "_self");
   };
-
-  const [user, setUser] = useState(null);
-
-  useEffect(() => {
-    const getUser = () => {
-      fetch("http://localhost:90/thirdpartyRouter/login/sucess", {
-        method: "GET",
-        credentials: "include",
-        headers: {
-          Accept: "application/json",
-          "Content-Type": "application/json",
-          "Access-Control-Allow-Credentials": true,
-        },
-      })
-        .then((response) => {
-          if (response.status === 200) return response.json();
-          throw new Error("authentication has been failed!");
-        })
-        .then((resObject) => {
-          setUser(resObject.user);
-        })
-        .catch((err) => {
-          console.log(err);
-        });
-    };
-    getUser();
-  }, []);
-
-  console.log(user);
 
   return (
     <div className="header">
