2b09bdd624e6f461e79707691e14d0dc4bd906ed
aayush <aayush172@gmail.com>
2022-12-26 21:35:31 +0545

    dropdown rent book and ebook

--- src/components/dashboard/MyBooks.jsx
+++ src/components/dashboard/MyBooks.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { FaBook } from "react-icons/fa";
+import "./dashboard.scss";
 import AddBook from "../add_book/AddBook";
 import BookCard from "../book-card/BookCard";
 import Box from "@mui/material/Box";
@@ -9,6 +10,60 @@ import Modal from "@mui/material/Modal";
 import axios from "axios";
 import Rentedbook from "../rentedBook-card/RentedBook";
 import ExchangeBookCard from "../exchangebook-card/ExchangeBookCard";
+
+import { styled, alpha } from "@mui/material/styles";
+
+import Menu from "@mui/material/Menu";
+import MenuItem from "@mui/material/MenuItem";
+import PendingIcon from "@mui/icons-material/Pending";
+import ThumbUpAltIcon from "@mui/icons-material/ThumbUpAlt";
+import ThumbDownIcon from "@mui/icons-material/ThumbDown";
+import KeyboardReturnIcon from "@mui/icons-material/KeyboardReturn";
+import MenuBookIcon from "@mui/icons-material/MenuBook";
+import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
+
+const StyledMenu = styled((props) => (
+  <Menu
+    elevation={0}
+    anchorOrigin={{
+      vertical: "bottom",
+      horizontal: "right",
+    }}
+    transformOrigin={{
+      vertical: "top",
+      horizontal: "right",
+    }}
+    {...props}
+  />
+))(({ theme }) => ({
+  "& .MuiPaper-root": {
+    borderRadius: 20,
+    marginTop: theme.spacing(1),
+    minWidth: 180,
+    color:
+      theme.palette.mode === "light"
+        ? "rgb(55, 65, 81)"
+        : theme.palette.grey[300],
+    boxShadow:
+      "rgb(255, 255, 255) 0px 0px 0px 0px, rgba(0, 0, 0, 0.05) 0px 0px 0px 1px, rgba(0, 0, 0, 0.1) 0px 10px 15px -3px, rgba(0, 0, 0, 0.05) 0px 4px 6px -2px",
+    "& .MuiMenu-list": {
+      padding: "4px 0",
+    },
+    "& .MuiMenuItem-root": {
+      "& .MuiSvgIcon-root": {
+        fontSize: 18,
+        color: theme.palette.text.secondary,
+        marginRight: theme.spacing(1.5),
+      },
+      "&:active": {
+        backgroundColor: alpha(
+          theme.palette.primary.main,
+          theme.palette.action.selectedOpacity
+        ),
+      },
+    },
+  },
+}));
 
 const style = {
   position: "absolute",
@@ -24,13 +79,31 @@ const style = {
 
 const MyBooks = () => {
   const [currentTab, setCurrentTab] = useState("listed");
+  const [currentButton, setCurrentButton] = useState("all");
   const [listedBooks, setListedBooks] = useState([]);
   const [rentedBooks, setRentedBooks] = useState([]);
+  const [rentedBookPending, setRentedBookPending] = useState([]);
+  const [rentedBookApproved, setRentedBookApproved] = useState([]);
+  const [rentedBookRejected, setRentedBookRejected] = useState([]);
+  const [rentedBookReturned, setRentedBookReturned] = useState([]);
   const [exchangedBooks, setExchangedBooks] = useState([]);
 
   const [open, setOpen] = React.useState(false);
   const handleOpen = () => setOpen(true);
   const handleClose = () => setOpen(false);
+
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const opened = Boolean(anchorEl);
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClosed = () => {
+    setAnchorEl(null);
+  };
+  const dropdownHandler = (tab) => {
+    setAnchorEl(null);
+    setCurrentButton(tab);
+  };
 
   const config = {
     headers: {
@@ -49,6 +122,27 @@ const MyBooks = () => {
       setRentedBooks(res.data.data);
       console.log(rentedBooks);
     });
+
+    axios
+      .get("http://localhost:90/rented_books/getpending", config)
+      .then((res) => {
+        setRentedBookPending(res.data.data);
+      });
+    axios
+      .get("http://localhost:90/rented_books/getApproved", config)
+      .then((res) => {
+        setRentedBookApproved(res.data.data);
+      });
+    axios
+      .get("http://localhost:90/rented_books/getRejected", config)
+      .then((res) => {
+        setRentedBookRejected(res.data.data);
+      });
+    axios
+      .get("http://localhost:90/rented_books/getReturned", config)
+      .then((res) => {
+        setRentedBookReturned(res.data.data);
+      });
     axios
       .get("http://localhost:90/user/exchange_requests", config)
       .then((res) => {
@@ -110,8 +204,97 @@ const MyBooks = () => {
       <div>
         {currentTab === "listed" &&
           listedBooks.map((book) => <BookCard book={book} />)}
-        {currentTab === "rented" &&
-          rentedBooks.map((book) => <Rentedbook book={book} />)}
+        {currentTab === "rented" && (
+          <div>
+            <div className="name-container">
+            <Button
+            className="name"
+              id="demo-customized-button"
+              aria-controls={opened ? "demo-customized-menu" : undefined}
+              aria-haspopup="true"
+              aria-expanded={opened ? "true" : undefined}
+              variant="contained"
+              disableElevation
+              onClick={handleClick}
+              endIcon={<KeyboardArrowDownIcon />}
+             
+            >
+              View By
+            </Button>
+            <StyledMenu
+              id="demo-customized-menu"
+              MenuListProps={{
+                "aria-labelledby": "demo-customized-button",
+              }}
+              anchorEl={anchorEl}
+              open={opened}
+              onClose={handleClosed}
+            >
+              <MenuItem
+                onClick={() => dropdownHandler("all")}
+                disableRipple
+                className={`tabs__tab ${
+                  currentButton === "all" && "tabs__tab--open"
+                }`}
+              >
+                <MenuBookIcon />
+                All
+              </MenuItem>
+              <MenuItem
+                onClick={() => dropdownHandler("pending")}
+                disableRipple
+                className={`tabs__tab ${
+                  currentButton === "pending" && "tabs__tab--open"
+                }`}
+              >
+                <PendingIcon />
+                Pending
+              </MenuItem>
+              <MenuItem
+                onClick={() => dropdownHandler("approved")}
+                disableRipple
+                className={`tabs__tab ${
+                  currentButton === "approved" && "tabs__tab--open"
+                }`}
+              >
+                <ThumbUpAltIcon />
+                Approved
+              </MenuItem>
+              <MenuItem
+                onClick={() => dropdownHandler("rejected")}
+                disableRipple
+                className={`tabs__tab ${
+                  currentButton === "rejected" && "tabs__tab--open"
+                }`}
+              >
+                <ThumbDownIcon />
+                Rejected
+              </MenuItem>
+              <MenuItem
+                onClick={() => dropdownHandler("returned")}
+                disableRipple
+                className={`tabs__tab ${
+                  currentButton === "returned" && "tabs__tab--open"
+                }`}
+              >
+                <KeyboardReturnIcon />
+                Returned
+              </MenuItem>
+            </StyledMenu>
+            </div>
+            {currentButton === "all" &&
+              rentedBooks.map((book) => <Rentedbook book={book} />)}
+            {currentButton === "pending" &&
+              rentedBookPending.map((book) => <Rentedbook book={book} />)}
+            {currentButton === "approved" &&
+              rentedBookApproved.map((book) => <Rentedbook book={book} />)}
+            {currentButton === "rejected" &&
+              rentedBookRejected.map((book) => <Rentedbook book={book} />)}
+            {currentButton === "returned" &&
+              rentedBookReturned.map((book) => <Rentedbook book={book} />)}
+          </div>
+        )}
+
         {currentTab === "exchanged" &&
           exchangedBooks.map((book) => <ExchangeBookCard book={book} />)}
       </div>
--- src/components/dashboard/MyEBook.jsx
+++ src/components/dashboard/MyEBook.jsx
@@ -1,16 +1,67 @@
 import React, { useState, useEffect } from "react";
-import { FaBook } from "react-icons/fa";
-import AddBook from "../add_book/AddBook";
-import BookCard from "../book-card/BookCard";
+import "./dashboard.scss";
+
 import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
 import Modal from "@mui/material/Modal";
 import axios from "axios";
 import Rentedbook from "../rentedBook-card/RentedBook";
-import ExchangeBookCard from "../exchangebook-card/ExchangeBookCard";
+import AutoStoriesIcon from '@mui/icons-material/AutoStories';
 import RentedEBook from "../rentedBook-card/RentedEBooks";
 import EbookCard from "../ebook-card/Ebook-card";
+import { styled, alpha } from "@mui/material/styles";
+import Menu from "@mui/material/Menu";
+import MenuItem from "@mui/material/MenuItem";
+import PendingIcon from "@mui/icons-material/Pending";
+import ThumbUpAltIcon from "@mui/icons-material/ThumbUpAlt";
+import ThumbDownIcon from "@mui/icons-material/ThumbDown";
+import KeyboardReturnIcon from "@mui/icons-material/KeyboardReturn";
+import MenuBookIcon from "@mui/icons-material/MenuBook";
+import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
+
+const StyledMenu = styled((props) => (
+  <Menu
+    elevation={0}
+    anchorOrigin={{
+      vertical: "bottom",
+      horizontal: "right",
+    }}
+    transformOrigin={{
+      vertical: "top",
+      horizontal: "right",
+    }}
+    {...props}
+  />
+))(({ theme }) => ({
+  "& .MuiPaper-root": {
+    borderRadius: 20,
+    marginTop: theme.spacing(1),
+    minWidth: 180,
+    color:
+      theme.palette.mode === "light"
+        ? "rgb(55, 65, 81)"
+        : theme.palette.grey[300],
+    boxShadow:
+      "rgb(255, 255, 255) 0px 0px 0px 0px, rgba(0, 0, 0, 0.05) 0px 0px 0px 1px, rgba(0, 0, 0, 0.1) 0px 10px 15px -3px, rgba(0, 0, 0, 0.05) 0px 4px 6px -2px",
+    "& .MuiMenu-list": {
+      padding: "4px 0",
+    },
+    "& .MuiMenuItem-root": {
+      "& .MuiSvgIcon-root": {
+        fontSize: 18,
+        color: theme.palette.text.secondary,
+        marginRight: theme.spacing(1.5),
+      },
+      "&:active": {
+        backgroundColor: alpha(
+          theme.palette.primary.main,
+          theme.palette.action.selectedOpacity
+        ),
+      },
+    },
+  },
+}));
 
 const style = {
   position: "absolute",
@@ -26,6 +77,11 @@ const style = {
 
 const MyEBooks = () => {
   const [currentTab, setCurrentTab] = useState("bought");
+  const [currentButton, setCurrentButton] = useState("all");
+  const [rentedEBookPending, setRentedEBookPending] = useState([]);
+  const [rentedEBookApproved, setRentedEBookApproved] = useState([]);
+  const [rentedEBookReading, setRentedEBookReading] = useState([]);
+  const [rentedEBookReturned, setRentedEBookReturned] = useState([]);
   const [boughtEBooks, setBoughtEBooks] = useState([]);
   const [rentedEBooks, setRentedEBooks] = useState([]);
   const [exchangedBooks, setExchangedBooks] = useState([]);
@@ -33,6 +89,19 @@ const MyEBooks = () => {
   const [open, setOpen] = React.useState(false);
   const handleOpen = () => setOpen(true);
   const handleClose = () => setOpen(false);
+
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const opened = Boolean(anchorEl);
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClosed = () => {
+    setAnchorEl(null);
+  };
+  const dropdownHandler = (tab) => {
+    setAnchorEl(null);
+    setCurrentButton(tab);
+  };
 
   const config = {
     headers: {
@@ -52,6 +121,21 @@ const MyEBooks = () => {
       setRentedEBooks(res.data.data);
       console.log(rentedEBooks);
     });
+    axios
+      .get("http://localhost:90/rented_ebooks/getReading", config)
+      .then((res) => {
+        setRentedEBookReading(res.data.data);
+      });
+    axios
+      .get("http://localhost:90/rented_ebooks/getPending", config)
+      .then((res) => {
+        setRentedEBookPending(res.data.data);
+      });
+    axios
+      .get("http://localhost:90/rented_ebooks/getReturned", config)
+      .then((res) => {
+        setRentedEBookReturned(res.data.data);
+      });
   }, []);
 
   return (
@@ -78,8 +162,85 @@ const MyEBooks = () => {
       <div>
         {currentTab === "bought" &&
           boughtEBooks.map((book) => <EbookCard book={book} />)}
-        {currentTab === "rented" &&
-          rentedEBooks.map((book) => <RentedEBook book={book} />)}
+        {currentTab === "rented" && (
+          <div>
+            <div className="name-container">
+              <Button
+                className="name"
+                id="demo-customized-button"
+                aria-controls={opened ? "demo-customized-menu" : undefined}
+                aria-haspopup="true"
+                aria-expanded={opened ? "true" : undefined}
+                variant="contained"
+                disableElevation
+                onClick={handleClick}
+                endIcon={<KeyboardArrowDownIcon />}
+              >
+                View By
+              </Button>
+              <StyledMenu
+                id="demo-customized-menu"
+                MenuListProps={{
+                  "aria-labelledby": "demo-customized-button",
+                }}
+                anchorEl={anchorEl}
+                open={opened}
+                onClose={handleClosed}
+              >
+                <MenuItem
+                  onClick={() => dropdownHandler("all")}
+                  disableRipple
+                  className={`tabs__tab ${
+                    currentButton === "all" && "tabs__tab--open"
+                  }`}
+                >
+                  <MenuBookIcon />
+                  All
+                </MenuItem>
+                <MenuItem
+                  onClick={() => dropdownHandler("pending")}
+                  disableRipple
+                  className={`tabs__tab ${
+                    currentButton === "pending" && "tabs__tab--open"
+                  }`}
+                >
+                  <PendingIcon />
+                  Payment Pending
+                </MenuItem>
+                <MenuItem
+                  onClick={() => dropdownHandler("reading")}
+                  disableRipple
+                  className={`tabs__tab ${
+                    currentButton === "reading" && "tabs__tab--open"
+                  }`}
+                >
+                  <AutoStoriesIcon />
+                  Reading
+                </MenuItem>
+                <MenuItem
+                  onClick={() => dropdownHandler("returned")}
+                  disableRipple
+                  className={`tabs__tab ${
+                    currentButton === "returned" && "tabs__tab--open"
+                  }`}
+                >
+                  <KeyboardReturnIcon />
+                  Returned
+                </MenuItem>
+              </StyledMenu>
+            </div>
+            {currentButton === "all" &&
+              rentedEBooks.map((book) => <RentedEBook book={book} />)}
+            {currentButton === "pending" &&
+              rentedEBookPending.map((book) => <RentedEBook book={book} />)}
+            {currentButton === "approved" &&
+              rentedEBookApproved.map((book) => <RentedEBook book={book} />)}
+            {currentButton === "reading" &&
+              rentedEBookReading.map((book) => <RentedEBook book={book} />)}
+            {currentButton === "returned" &&
+              rentedEBookReturned.map((book) => <RentedEBook book={book} />)}
+          </div>
+        )}
       </div>
     </div>
   );
